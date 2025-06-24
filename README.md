4b6ea0c1e342559cb5e81f6af8b708f6a71516bf
rivka-flig <rivkaflig@gmail.com>
2025-06-24 13:58:07 -0400

    Revert "Fixed merge conflict for page routing" Undid my merge conflict fix - did it wrong This reverts commit d8642750baa67256be0713f39ee20c80122cbb4f, reversing changes made to 68fe3b97b80d424d62292a9ad51a1870bbe98c55.

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # TuneOut
 
 
-## Description: 
-### **View all your favorite songs in one place!**
-
-#### *Add and remove songs to each playlist, and add and remove playlists.*
+## Description: *** View all your favorite songs in one place! ***
+** Add and remove songs to each playlist, and add and remove playlists. **
+==========
 
 ### Authors:
 * Leah Feldman
@@ -12,8 +11,9 @@
 * Hailey Lazar
 * Toby Meth
 
-> ### **Sample Information Format**
-> # Playlist Titles
-> ## Songs
-> * Artist 
-> * Release Date
+** Sample Information of Website Format **
+
+# Playlist Titles
+## Songs
+* Artist 
+* Release Date
--- TuneOut/src/App.jsx
+++ TuneOut/src/App.jsx
@@ -1,31 +1,12 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SiteHeader from './components/SiteHeader';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Playlists from './pages/Playlists';
-=======
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './Home.jsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//add when add these pages:
-//import Header from './Header';
-//import About from './pages/About';
-//import Contact from './pages/Contact';
-//add to routes:
-  //<Header />
-//<Route path="/about" element={<About />} />
-//<Route path="/contact" element={<Contact />} />
-
->>>>>>> 1ebc70c5e2e5375e883e57d0b2dd726a5a1c7746
 
 function App() {
   return (
-<<<<<<< HEAD
     <Router>
       <SiteHeader />
       <div className="container mt-4">
@@ -38,17 +19,6 @@ function App() {
       </div>
     </Router>
   );
-=======
-    <>
-      <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        
-      </Routes>
-    </Router>
-    </>
-  )
->>>>>>> 1ebc70c5e2e5375e883e57d0b2dd726a5a1c7746
 }
 
 export default App;
